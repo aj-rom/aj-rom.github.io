00b1077050263d1b7711d2e0938a0b0954b3d0b1
A.J. Romaniello <ajiellodev@gmail.com>
2021-05-25 10:28:26 -0600

    Add basic info

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,13 +4,12 @@ export default class Jumbotron extends Component {
     render() {
         return (
             <div className="jumbotron">
-                <h1 className="display-4">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra
-                    attention to featured content or information.</p>
-                <p>It uses utility classes for typography and spacing to space content out within the larger
-                    container.</p>
+                <h1 className="display-4">Welcome!</h1>
+                <p className="lead">This page is still a work in progress... bare with me while I build this out to be an awesome portfolio!</p>
+                <p>We will be up and running in no time.</p>
                 <p className="lead">
-                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                    <a className="btn btn-primary btn-md" href="https://github.com/aj-rom" role="button">My GitHub</a>
+                    <a className="btn btn-primary btn-md" href="https://linkedin.com/aj-romaniello" role="button">Linked In</a>
                 </p>
             </div>
         )
