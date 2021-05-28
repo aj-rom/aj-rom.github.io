58b54cb99b9a8c87e9ee9d6e84e7b6d1524c8293
A.J. Romaniello <ajiellodev@gmail.com>
2021-05-28 01:16:28 -0600

    Add theme switcher

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react'
 
-export default class Jumbotron extends Component {
+export default class Body extends Component {
     render() {
         return (
-            <div className="jumbotron">
-                <h1 className="display-4">Welcome!</h1>
-                <p className="lead">This page is still a work in progress... bare with me while I build this out to be an awesome portfolio!</p>
-                <p>We will be up and running in no time.</p>
-                <p className="lead">
-                    <a className="btn btn-primary btn-md" href="https://github.com/aj-rom" role="button">My GitHub</a>
-                    <a className="btn btn-primary btn-md" href="https://www.linkedin.com/in/aj-romaniello" role="button">Linked In</a>
-                </p>
-            </div>
+            <header id='jumbotron'>
+                <hgroup>
+                    <h1>Welcome!</h1>
+                    <h2>This page is still a work in progress. Bare with me while I build this out to be an awesome portfolio!</h2>
+                </hgroup>
+                <nav>
+                    <ul>
+                        <li>
+                            <a className="outline" href="https://github.com/aj-rom" role="button">My GitHub</a>
+                        </li>
+                        <li>
+                            <a className="outline" href="https://www.linkedin.com/in/aj-romaniello" role="button">Linked In</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <details open={true}>
+                    <summary>Switch Theme</summary>
+                    <nav>
+                        <ul>
+                            <li>Theme:</li>
+                            <li><a href="#" onClick={this.switchToAuto}>Auto</a></li>
+                            <li><a href="#" onClick={this.switchToLight}>Light</a></li>
+                            <li><a href="#" onClick={this.switchToDark}>Dark</a></li>
+                        </ul>
+                    </nav>
+                </details>
+
+            </header>
         )
     }
+
+    switchToAuto = () => {
+        changeTheme('auto')
+    }
+
+    switchToLight = () => {
+        changeTheme('light')
+    }
+
+    switchToDark = () => {
+        changeTheme('dark')
+    }
+}
+
+function changeTheme(theme) {
+    const node = document.querySelector('html')
+    node.dataset.theme = theme
+    return node
 }
