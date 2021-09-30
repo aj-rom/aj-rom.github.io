57fe8975edbfbca1743eaadfb585004497c4eaf8
A.J. Romaniello <ajiellodev@gmail.com>
2021-09-29 23:19:54 -0600

    Stub out footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <footer>This the main body footer</footer>
-        )
-    }
+const Footer = () => {
+    return (
+        <footer>
+            <section className='grid'>
+                <article>
+                    <hgroup>
+                        <h2>Find Me</h2>
+                        <h3>Links to some important websites that I frequent.</h3>
+                    </hgroup>
+
+                    <ul>
+                        {listLink('https://linkedin.com/ajromaniello', 'LinkedIn')}
+                        {listLink('https://github.com/aj-rom', 'GitHub')}
+                        {listLink('https://dev.com/ajrom', 'Blog')}
+                    </ul>
+                </article>
+
+                <article>
+                    <hgroup>
+                        <h2>Contact</h2>
+                        <h3>How to get in touch with me.</h3>
+                    </hgroup>
+                    <p>The best way to reach me is via email:</p>
+                    <ul>
+                        {listLink('mailto:ajromaniello@gmail.com?subject=Mail%20From%20Portfolio', 'ajromaniello@gmail.com')}
+                    </ul>
+                </article>
+            </section>
+        </footer>
+    )
 }
+
+function listLink(link, text, props = {}) {
+    return <li><a href={link} {...props}>{text}</a></li>
+}
+
+export default Footer
