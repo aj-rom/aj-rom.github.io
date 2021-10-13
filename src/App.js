f8cd09ef15330ff7b9b82e65a1e5cea74e781fd3
A.J. Romaniello <ajiellodev@gmail.com>
2021-10-12 19:02:39 -0600

    add theme and index css files into react pipeline

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import { HashRouter } from "react-router-dom";
 import SwitchContainer from "./containers/SwitchContainer";
 
+import './style/index.css'
 const App = () => {
     return (
         <div className='container'>
