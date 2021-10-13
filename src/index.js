36e8288ae009b54f296ad54095d4c685a2a0ebcc
A.J. Romaniello <ajiellodev@gmail.com>
2021-10-12 23:17:48 -0600

    switch to google tag manager package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk'
 import rootReducer from "./reducers/rootReducer";
+import App from './App'
+import TagManager from 'react-gtm-module';
 import './style/theme/default.css'
+
 const store = createStore(rootReducer, applyMiddleware(thunk))
+const tagManager = { gtmId: 'G-GG59QNM49Y' }
+TagManager.initialize(tagManager)
 
 ReactDOM.render(
   <Provider store={store}>
